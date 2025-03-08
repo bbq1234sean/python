3fd7e87c81ddc60c9741bc6fbfe9256282899937
bbq1234sean <seanhsieh1013@gmail.com>
2025-03-08 09:32:14 +0800

    class1

--- class1/class1.py
+++ class1/class1.py
@@ -1,1 +1,20 @@
+# 這是單行註解
+# ctrl+?可以切換框選範圍變成註解
+"""
+這是多行註解1
+這是多行註解2
+這是多行註解3
+"""
 
+print("Hello World")  # 這是把字串印出來的指令
+# 基本型態
+print(1)  # 整數 int
+print(1.1)  # 浮點數 float
+print("Hello World")  # 字串 string
+print(True)  # 布林值 boolean
+print(None)  # 空值 None
+
+# 變數 variabie
+a = 1  # 把整數存到電腦記憶體中,並且取名為a
+# =的功能是將右邊資料存到變數
+print(a)  # 把a這個變數印出來
