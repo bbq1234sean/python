e470ed31946784403b65be109ab36d2a93f39099
bbq1234sean <seanhsieh1013@gmail.com>
2025-03-08 12:05:21 +0800

    class2

--- class2/class2.py
+++ class2/class2.py
@@ -81,6 +81,6 @@ print(1**1)  # 次方
 
 # 正方形面積計算
 r = input("請輸入正方形邊長:")
-print(r**2)
-area = r * 2
+r = int(r)
+area = r * r
 print(f"正方形的面積為{area}")
---
+++ class2/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+# 正方形面積計算
+r = input("請輸入正方形邊長:")
+r = int(r)
+area = r * r
+print(f"正方形的面積為{area}")
