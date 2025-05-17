54ce3c9e3bda88177c4de155d8bbe1a98d4f5711
bbq1234sean <seanhsieh1013@gmail.com>
2025-05-17 08:31:23 +0800

    hw13

--- class9/hw13.py
+++ class9/hw13.py
@@ -20,6 +20,32 @@ EX:
 恭喜猜中!
 """
 
+password = 24
+
+Upper = 100
+Lower = 0
+num = int(input("請輸入0~100的整數:"))  # 取得輸入的數字
+
+while True:
+
+    if num < 0 or num > 100:
+        num = int(input("請輸入0~100範圍內的整數:"))
+        continue
+    elif num > password:
+        Upper = num
+        print("再小一點")
+        num = int(input("請輸入" + str(Lower) + "~" + str(Upper) + "的整數:"))
+        continue
+    elif num < password:
+        Lower = num
+        print("再大一點")
+        num = int(input("請輸入" + str(Lower) + "~" + str(Upper) + "的整數:"))
+        continue
+    else:
+        print("恭喜猜中!")
+        break
+
+
 L = ["1.蘋果汁", "2.柳橙汁", "3.葡萄汁", "4.系統關閉"]
 print(L)
 
--- class9/tempCodeRunnerFile.py
+++ class9/tempCodeRunnerFile.py
@@ -1,2 +1,24 @@
+password = 24
 
-    ans = input("請輸入編號:")
+Upper = 100
+Lower = 0
+num = int(input("請輸入0~100的整數:"))  # 取得輸入的數字
+
+while True:
+
+    if num < 0 or num > 100:
+        num = int(input("請輸入0~100範圍內的整數:"))
+        continue
+    elif num > password:
+        Upper = num
+        print("再小一點")
+        num=int(input("請輸入" + str(Lower) + "~" + str(Upper) + "的整數:"))
+        continue
+    elif num < password:
+        Lower = num
+        print("再大一點")
+        num = int(input("請輸入" + str(Lower) + "~" + str(Upper) + "的整數:"))
+        continue
+    else:
+        print("恭喜猜中!")
+        break
